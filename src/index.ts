05b9685ce1490df6390e92c1228c467e0e2e94e2
Yadu <yadu.jain@tech9.com>
2023-09-16 20:12:39 +0530

    add base package logger

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export class Log {
+  static success = (message: string) => console.log(`%c ${message}`, 'color: green');
+
+  static danger = (message: string) => console.log(`%c ${message}`, 'color: red');
+
+  static info = (message: string) => console.log(`%c ${message}`, 'color: black');
+}
